7a2602cc6f344f1a638a7105195d276363d74892
productionbaaz <productionbaaz@gmail.com>
2026-08-31 18:46:50 +0500

    upload new files

---
+++ src/main-view.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import PublicSheetPage from './components/PublicSheetPage.jsx';
+import './styles.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <PublicSheetPage />
+  </React.StrictMode>
+);
